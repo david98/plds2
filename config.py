911cc38287c07fa7a6dceaa4d66142dfd906740c
Davide Volta <davidevolta98@gmail.com>
2019-09-14 14:12:13 +0200

    added logging, installed python-telegram-bot

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 log_file = './plds.log'
 log_level = DEBUG
+time_between_usb_checks = 5
+power_outage_message = "POWEROUTAGE"
+power_back_message = "POWERBACK"
+max_message_length = 1000
+ser_device = "/dev/ttyACM0"
+baud_rate = 9600
+arduino_vendor_id = 9025
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import usb.core
 import logging
 import config as cfg
 from enum import Enum
+import time
 
 
 class USBDetector:
@@ -20,7 +21,9 @@ class USBDetector:
             if device.action == 'add':
                 dev = usb.core.find(idVendor=self.vendor_id)
                 if dev is not None:
+                    logging.debug('USB Detector: new USB device detected.')
                     self.on_detection()
+            time.sleep(cfg.time_between_usb_checks)
 
 
 class Status(Enum):
@@ -31,13 +34,6 @@ class Status(Enum):
 
 class PLDS:
 
-    POWER_OUTAGE_MESSAGE = "POWEROUTAGE"
-    POWER_BACK_MESSAGE = "POWERBACK"
-    MAX_MESSAGE_LENGTH = 1000
-    SER_DEVICE = "/dev/ttyACM0"
-    BAUD_RATE = 9600
-    ARDUINO_VENDOR_ID = 9025
-
     def __init__(self):
         self.status: Status = Status.NO_SENSOR
         self.ser = None
@@ -45,45 +41,48 @@ class PLDS:
     def on_power_outage(self):
         if self.status is not Status.ALARM:
             self.status = Status.ALARM
-            print('POWER OUTAGE')
+            logging.warning('Power outage detected.')
         pass
 
     def on_power_back(self):
         if self.status is not Status.NORMAL:
             self.status = Status.NORMAL
-            print('POWER BACK')
+            logging.warning('Power came back.')
         pass
+
+    def on_connection_lost(self):
+        logging.error('Sensor disconnected.')
+        self.status = Status.NO_SENSOR
+        self.ser.close()
+        self.wait_for_sensor()
 
     def wait_for_data(self):
         if self.status is not Status.NO_SENSOR:
             try:
                 while True:
-                    data = str(self.ser.readline(1000))
-                    if PLDS.POWER_OUTAGE_MESSAGE in data:
+                    data = str(self.ser.readline(cfg.max_message_length))
+                    if cfg.power_outage_message in data:
                         self.on_power_outage()
-                    elif PLDS.POWER_BACK_MESSAGE in data:
+                    elif cfg.power_back_message in data:
                         self.on_power_back()
             except serial.SerialException:
                 self.on_connection_lost()
         else:
             self.wait_for_sensor()
 
-    def on_connection_lost(self):
-        print('DISCONNECTED')
-        self.status = Status.NO_SENSOR
-        self.ser.close()
-        self.wait_for_sensor()
-
     def try_connection(self):
         try:
-            self.ser = serial.Serial(PLDS.SER_DEVICE, PLDS.BAUD_RATE)
+            self.ser = serial.Serial(cfg.ser_device, cfg.baud_rate)
             self.status = Status.NORMAL
+            logging.info('Connection to sensor established.')
             self.wait_for_data()
         except serial.SerialException:
+            logging.debug('Connection failed, probably tried with the wrong device.')
             self.wait_for_sensor()
 
     def wait_for_sensor(self):
-        detector = USBDetector(PLDS.ARDUINO_VENDOR_ID, self.try_connection)
+        logging.debug('Starting USB detector...')
+        detector = USBDetector(cfg.arduino_vendor_id, self.try_connection)
         detector.listen()
 
     def start(self):
@@ -91,8 +90,9 @@ class PLDS:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename=cfg.log_file, level=cfg.log_level, filemode='a+', format='%(asctime)s - %(levelname)s:: %(message)s')
-    logging.debug('PLDS started.')
+    logging.basicConfig(filename=cfg.log_file, level=cfg.log_level, filemode='a+',
+                        format='%(asctime)s - %(levelname)s:: %(message)s')
+    logging.info('PLDS started.')
     plds = PLDS()
     plds.start()
 
